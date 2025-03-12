3ef443724ac3b2130a642b55418d01e4e70b82da
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-03-12 00:15:41 -0700

    Adding admin file

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Project, Skill, Education, Experience, Contact
+
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Contact)
