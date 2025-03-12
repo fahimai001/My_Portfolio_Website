69d012541b950aa0365d7311ba8202f7cb138062
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-03-12 00:13:20 -0700

    Adding url file

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -20,3 +20,18 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('portfolio.urls')),  # Include portfolio app URLs
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
